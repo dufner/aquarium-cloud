6e7ecda6d1291a1e59816459911bf4853057ce43
vik <dufner@mail.ru>
2024-06-14 12:15:12 +0300

    connect to database

--- aquarium-cloud/src/main/java/tacos/Aquarium.java
+++ aquarium-cloud/src/main/java/tacos/Aquarium.java
@@ -1,4 +1,5 @@
 package tacos;
+import java.util.Date;
 import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -6,6 +7,11 @@ import lombok.Data;
 
 @Data
 public class Aquarium {
+
+
+  private Long id;
+
+  private Date createdAt= new Date();
 
   @NotNull
   @Size(min=5, message="Name must be at least 5 characters long")
